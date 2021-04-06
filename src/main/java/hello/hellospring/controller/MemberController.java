27d233dcd88f25a81cb5e6ad0a22cfdd5c480a72
JongGurlHan <gojkhan13@gmail.com>
2021-04-06 22:08:33 +0900

    04.06 회원 조회

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -16,5 +22,29 @@ public class MemberController {
 
     //생성자에 @Autowired가 있으면 MemberController가 생성될때
     // 스프링빈에 등록되어있는 memberService를 스프링 컨테이너에 있는 memberService에 연결시켜준다
-    //
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+        public String list (Model model){
+            List<Member> members = memberService.findMembers();
+            model.addAttribute("members", members);
+            return "/members/memberList";
+
+
+        }
+
 }
